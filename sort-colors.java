2e0ffcbb3eb450f867bdd3f0c10795e9cabed709
AbhineeT-D7 <abhinetakg78965@gmail.com>
2025-08-16 03:22:35 +0530

    sort-colors

---
+++ sort-colors.java
@@ -0,0 +1,36 @@
+// Problem : https://leetcode.com/problems/sort-colors/
+class Solution {
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+
+        int i = low - 1;
+
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp1 = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp1; 
+        return i + 1;
+    }
+    
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+    public void sortColors(int[] nums) {
+
+        int n = nums.length;
+
+        quickSort(nums, 0, n - 1);
+
+    }
+}
