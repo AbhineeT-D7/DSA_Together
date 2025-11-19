27dce8eb0a38c0772191267c5403e42aeb738d0f
AbhineeT-D7 <abhinetakg78965@gmail.com>
2025-11-19 19:51:18 +0530

    Keep-multiplying-found-values-by-two

---
+++ Keep-multiplying-found-values-by-two.java
@@ -0,0 +1,17 @@
+// Problem : https://leetcode.com/problems/keep-multiplying-found-values-by-two/?envType=daily-question&envId=2025-11-19
+
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int n : nums){
+            set.add(n);
+        }
+
+        while(set.contains(original)){
+            original*=2;
+        }
+
+        return original;
+        
+    }
+}
